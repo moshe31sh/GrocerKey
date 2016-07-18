7ed874989f7e076d69f50f06a80c9d2590330df6
moshe <moshe31sh@gmail.com>
2016-07-19 01:04:06 +0300

    start building asyc download for cat image

--- GrocerKey/app/src/main/java/com/grocerkey/grocerkey/fragments/CatFrag.java
+++ GrocerKey/app/src/main/java/com/grocerkey/grocerkey/fragments/CatFrag.java
@@ -1,14 +1,21 @@
 package com.grocerkey.grocerkey.fragments;
 
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.grocerkey.grocerkey.R;
+import com.grocerkey.grocerkey.utiles.AppConsts;
+import com.grocerkey.grocerkey.utiles.DownloadImageTask;
+
 
 /**
  * Created by moshe on 18-07-16.
@@ -18,8 +25,9 @@ import com.grocerkey.grocerkey.R;
  * cat fragment  - tab no 2
  */
 public class CatFrag extends Fragment {
-    View rootView;
-
+    private View rootView;
+    private TextView tvKitten;
+    private ImageView ivCat;
 
     /**
      * Create fragment instance
@@ -35,6 +43,25 @@ public class CatFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater,
                              @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_cat, container, false);
+        init();
         return rootView;
     }
+
+    /**
+     * This method init vars
+     */
+    public void init(){
+        this.tvKitten = (TextView) rootView.findViewById(R.id.cat_text_id);
+        this.ivCat = (ImageView) rootView.findViewById(R.id.cat_image_view_id);
+        this.tvKitten.setText(AppConsts.cuteKitten);
+        new DownloadImageTask(ivCat).execute(AppConsts.catPicUrl);
+
+    }
+
+//    @Override
+//    public void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        this.cat = new Cat();
+//       // Bitmap bitmap = null;
+//    }
 }
--- GrocerKey/app/src/main/java/com/grocerkey/grocerkey/utiles/AppConsts.java
+++ GrocerKey/app/src/main/java/com/grocerkey/grocerkey/utiles/AppConsts.java
@@ -10,5 +10,13 @@ package com.grocerkey.grocerkey.utiles;
  */
 public final class AppConsts {
     public static final String helloWorld = "Hello,World!";
+    public static final String cuteKitten = "A Very Cute Kitten:";
+
+
+    /***URLS****/
+
+    public static final String catPicUrl = "www.sfgov2.org/ftp/uploadedimages/acc/Adoption_Center/Foster%20Kitten%2012007.jpg";
+
+
 
 }
